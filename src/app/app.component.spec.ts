f67fd9ac81c4e565eb6c0375887723ec00ff3a4f
Rodzman <designrsp@gmail.com>
2018-04-02 02:39:24 -0300

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,28 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { FooterComponent } from './footer/footer.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { PostUnitComponent } from './post-list/post-unit/post-unit.component';
+import { FilterPipe } from './filter.pipe';
+import { ReplacePipe } from './replace.pipe';
+import { PostsService } from './posts.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        PostListComponent,
+        FooterComponent,
+        PostUnitComponent,
+        FilterPipe,
+        ReplacePipe
       ],
+      imports: [ ReactiveFormsModule, HttpClientModule ],
+      providers: [PostsService]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -18,10 +35,11 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
   }));
-  it('should render title in a h1 tag', async(() => {
+  it('should return searchText from searchBox from header', () => {
+    let searchText = 'test';
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+    const app = fixture.debugElement.componentInstance;
+    app.rescueSearch(searchText)
+    expect(app.searchText).toBe(searchText);
+  });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { PostsService } from './posts.service';
+import { FilterPipe } from './filter.pipe';
+import { ReplacePipe } from './replace.pipe';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostUnitComponent } from './post-list/post-unit/post-unit.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PostListComponent,
+    PostUnitComponent,
+    FilterPipe,
+    ReplacePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [PostsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
